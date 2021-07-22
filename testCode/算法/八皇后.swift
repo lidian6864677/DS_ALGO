dd0eaa026e4c8f8069093213641205f3dea79b2f
Dian。 <lidian6864677@163.com>
2021-07-22 21:03:30 +0800

    八皇后 汉诺塔

---
+++ testCode/算法/八皇后.swift
@@ -0,0 +1,30 @@
+//
+//  八皇后问题.swift
+//  算法
+//
+//  Created by DianLee on 2021/7/20.
+//
+
+import Cocoa
+
+/**
+ 题目
+ 
+ */
+
+func 八皇后(){
+//    let  n = 3
+    check(title: "八皇后") {
+        
+    }
+
+}
+
+func chooseQuen(_ n: NSInteger) -> [[Int:Int]]{
+    
+    
+    
+    
+    
+    return [[1:3]]
+}
--- testCode/算法/汉诺塔.swift
+++ testCode/算法/汉诺塔.swift
@@ -11,30 +11,51 @@ import Cocoa
   --|--              |                      |
  ----|----           |                      |
 ------|------         |                      |
-讲A中的盘子挪动到C
- (!image)(#imageLiteral(resourceName: "negative_weight_01.png"))
+将A中的盘子挪动到C
  
+ 1 个
+ - 1a ~ c
+ 2 个
+ - 1a~ b
+ - 2a~ c
+ - 1b~ c
+ 3 个
+ - 1a~ c
+ - 2a~ b
+ - 1c~ b
+ - 3a~ c
+ - 1b~ a
+ - 2b~ c
+ - 1a~ c
  
  */
 
 func 汉诺塔(){
-    let  n = 25
+    let  n = 3
     check(title: "汉诺塔") {
-        print(climb01(n))
+        print(hanoi01(n, "A", "B", "C"))
     }
-    check(title: "汉诺塔") {
-        print(climb02(n))
-    }
+
 }
 
 
-func hanoi01(_ n: NSInteger) -> NSInteger {
-    if n == 1 { return n }
-    return n
+/// 汉诺塔
+/// - Parameters:
+///   - n: 层数
+///   - p1: 第一个柱子
+///   - p2: 第二个柱子
+///   - p3: 第三个柱子
+/// - Returns:
+func hanoi01(_ n: NSInteger, _ p1: String, _ p2: String, _ p3: String) {
+    if n <= 1 {
+        move(n, from: p1, to: p3)
+        return
+    }
+    hanoi01(n-1, p1, p3, p2)
+    move(n, from: p1, to: p3)
+    hanoi01(n-1, p2, p1, p3)
 }
 
-func hanoi02(_ n: NSInteger) -> NSInteger {
-    
-    
-    return n
+func move(_ n: NSInteger, from: String, to: String ) {
+    print("盘子:  \(n)  从 \(from) ---> \(to)")
 }
